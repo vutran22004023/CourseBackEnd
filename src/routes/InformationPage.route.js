9757eb378b9d5776c9d5358c8b10751d56ee03ba
VuTran <levu1962004@gmail.com>
2024-10-29 12:29:01 +0700

    fix bug cache

--- src/routes/InformationPage.route.js
+++ src/routes/InformationPage.route.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import informationPageController from '../controllers/informationPage.controller.js';
-import CacheUtility from '../utils/cache.util.js'
-import { AuthMiddleware } from '../middlewares/index.js';
+import { AuthMiddleware,CacheMiddleware } from '../middlewares/index.js';
 const router = express.Router();
 
-router.get('/', CacheUtility.getCache, informationPageController.get);
+router.get('/', CacheMiddleware.getCache, informationPageController.get);
 router.put('/update-information',AuthMiddleware.authAdmin, informationPageController.put);
 
 export default router;
