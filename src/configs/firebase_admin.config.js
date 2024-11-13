b7d38b9d3d9ba3f7e45ddc9e91240d2bae16ef6b
Nguyen Lam <nlsc1999@gmail.com>
2024-11-13 22:37:09 +0700

    improve read json file

--- src/configs/firebase_admin.config.js
+++ src/configs/firebase_admin.config.js
@@ -1,8 +1,20 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../../firebase-cert.json' assert { type: 'json' };
+import fs from 'fs';
+import path from 'path';
+import logger from '../configs/logger.config.js';
+import { fileURLToPath } from 'url';
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, '../../firebase-cert.json');
+
+try {
+  const data = fs.readFileSync(filePath, 'utf8');
+  const serviceAccount = JSON.parse(data);
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+} catch (err) {
+  logger.error(err);
+}
 
 export default admin;
